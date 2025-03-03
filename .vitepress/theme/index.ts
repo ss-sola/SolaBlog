f962678d0e4caf3395bf636cf4272e9df95fc9de
sola <2451477516@qq.com>
2025-03-03 21:05:43 +0800

    解决打包时没有document问题

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -6,10 +6,11 @@ const onlyMd=[`%E5%85%B3%E4%BA%8EFreeMovie`]
 function includeOnlyMd(to: any, from: any) {
   return  onlyMd.some(item=> to.includes(item))
 }
-
 export default {
   ...DefaultTheme,
   enhanceApp({ app,router }) {
+    /**@ts-ignore */
+    if (import.meta.env.SSR) return; // 避免 SSR 访问 document
     // 监听每次路由变化后触发
     router.onAfterRouteChanged = (to: any, from: any) => {
       console.log(to,from)
