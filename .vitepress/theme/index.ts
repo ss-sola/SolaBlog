1909587eb5a672f00621fc5ffcc694082f67a937
sola <2451477516@qq.com>
2025-03-03 20:46:15 +0800

    优化关于页

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,19 +1,23 @@
 import DefaultTheme from 'vitepress/theme'
 import './styles.css'
 
+const onlyMd=[`%E5%85%B3%E4%BA%8EFreeMovie`]
+
+function includeOnlyMd(to: any, from: any) {
+  return  onlyMd.some(item=> to.includes(item))
+}
+
 export default {
   ...DefaultTheme,
   enhanceApp({ app,router }) {
     // 监听每次路由变化后触发
-    // router.onAfterRouteChanged = (to:any) => {
-    //   console.log(to)
-    //   const hash = decodeURIComponent(to.hash)
-    //   if (hash) {
-    //     const targetElement = document.querySelector(hash)
-    //     if (targetElement) {
-    //       targetElement.scrollIntoView({ behavior: 'smooth' })
-    //     }
-    //   }
-    // }
+    router.onAfterRouteChanged = (to: any, from: any) => {
+      console.log(to,from)
+      if (includeOnlyMd(to,from)) {
+        document.documentElement.classList.add('custom-hidden');
+      } else {
+        document.documentElement.classList.remove('custom-hidden');
+      }
+    };
   }
 }
